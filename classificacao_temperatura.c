aad16c3db3193837241a15ccd1c6efd83beafaa1
Felipe Almeida <felipealmeida281997@hotmail.com>
2018-04-14 15:26:06 -0300

    Add files via upload

---
+++ classificacao_temperatura.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+
+int temperatura;
+
+printf("Digite a temperatura: ");
+  scanf("%i", &temperatura);
+    if (temperatura >= 30)
+      printf("A temperatura está muito quente%c");
+      else
+      printf("A temperatura está relativamente boa%c");
+   }
