d9b856878e6eb1f9b4751fd0efba1a3dab01b395
Felipe Almeida <felipealmeida281997@hotmail.com>
2018-04-14 15:01:20 -0300

    Add files via upload
    Soma entre três números

---
+++ Soma_entre_3numeros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+
+int n1, n2, n3;
+  printf("Digite um número:");
+  scanf("%i", &n1);
+  printf("Digite o Segundo Número: ");
+  scanf("%i", &n2);
+  printf("Digite o Terceiro Número: ");
+  scanf("%i", &n3);
+  printf("O resultado da soma é: %i", n1 + n2 + n3);
+}
