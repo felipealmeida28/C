d7aa659901b7228693b503f365f01c590c7b10d0
Felipe Almeida <felipealmeida281997@hotmail.com>
2018-04-14 12:34:29 -0300

    Add files via upload
    Pedir um número que sera calculado o Quadrado e ao Cubo

---
+++ Quadrado_Cubo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+  int a;
+  printf("Digite Um Número: \n");
+  scanf("%i", &a );
+  printf("O resultado ao Quadrado é: %i\n O resultado ao cubo é: %i\n", a * a, a * a * a);
+  return 0;
+
+}
