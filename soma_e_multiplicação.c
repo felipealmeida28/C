872350a9aa6f65f942706ff1eab13fe8f16e7ff6
Felipe Almeida <felipealmeida281997@hotmail.com>
2018-04-14 15:11:21 -0300

    Add files via upload

---
+++ soma_e_multiplicação.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+
+int n1, n2, n3;
+printf("Digite o Primeiro Número: ");
+scanf("%i", &n1);
+printf("Digite o Segundo Número: ");
+scanf("%i", &n2);
+printf("Digite o Terceiro Número: ");
+scanf("%i", &n3);
+printf("O resultado da soma é: %i\n O resultado da MUltiplicação é: %i", n1 + n2 + n3, n1 * n2 * n3);
+
+}
