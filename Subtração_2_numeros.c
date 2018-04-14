c1ec1d6b45ac84269de24058f23e85f56916ab83
Felipe Almeida <felipealmeida281997@hotmail.com>
2018-04-14 15:17:28 -0300

    Add files via upload

---
+++ Subtração_2_numeros.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+
+int n1, n2;
+printf("Digite o Primeiro Número: ");
+scanf("%i",&n1);
+printf("Digite o Segundo Número: ");
+scanf("%i",&n2);
+printf("O resultado da Subtração é: %i", n1 - n2);
+
+}
